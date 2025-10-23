369077b56e163c24f325fbd7f36d794aae3f3c52
mawardanichelseaarum-coder <mawardanichelseaarum@gmail.com>
2025-10-22 20:14:55 -0700

    Add button click event to show alert message

---
+++ script.js/script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolsapa = dokumen.getElementById('sapaButton');
+//2. Tambhakan 'even listener' untuk aksi 'clik'
+tombolsapa. addEventListener('click',fuction() {
+      // 3. Jalankan kode ini ketika tombol di-klik
+      alert('halo! Terima kasih sudah berkunjung');
+});
